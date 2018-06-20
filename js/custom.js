329499b8a7878c8569fba764185f01563bcedcb3
Urak <33769257+UrakFerraz@users.noreply.github.com>
2018-06-20 13:19:27 -0300

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -3,22 +3,24 @@
 	var $fotoTopo = $('#foto-topo-anime');
 	TweenMax.from($fotoTopo, 0.5, {opacity: 0,
 									y: -200,
+									delay: 1,
 									ease:Expo.easeIn});
 
 	var $header = $('#header');
 	TweenLite.from($header, 0.5, {opacity: 0, x: -200,
 									ease:Expo.easeIn,
-									delay: 0.1});
+									delay: 1});
 
 	var $toptext = $('#anime-top-text');
 	TweenLite.from($toptext, 0.5, {opacity: 0,
 									x: -200,
 									ease:Expo.easeIn,
-									delay: 0.3});
+									delay: 1});
 	
 	var $redShape = $('#red-shape-anime-entry');
 	TweenMax.to($redShape, 0.8, {scaleY: 0,
 								y: 450,
+								delay: 1,
 								ease:Expo.easeOut});
 
 })(jQuery);
